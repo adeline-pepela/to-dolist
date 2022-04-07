f49d5d441ede7519750df2669876279c360e087a
adeline-pepela <adelinemakokha@gmail.com>
2022-04-07 08:57:18 +0300

    adding description

--- to-dolist/src/app/app.module.ts
+++ to-dolist/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    TaskDetailComponent
   ],
   imports: [
     BrowserModule,
